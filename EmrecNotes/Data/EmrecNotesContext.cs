fa63d2e83ce35abf669cb5472eda77888073f832
Boethius2001 <emrecan.senel2001@gmail.com>
2025-03-16 22:49:29 +0300

    DbContext Created

---
+++ EmrecNotes/Data/EmrecNotesContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmrecNotes.Data
+{
+    public class EmrecNotesContext : DbContext
+    {
+        public EmrecNotesContext(DbContextOptions<EmrecNotesContext> Options)
+        :base(Options)
+        {
+        }
+
+        public DbSet<EmrecNotes.Models.Account> Accounts { get; set; }
+    }
+}
