407f3077042896fdea865b8718b2dba7fff3e3fb
Boethius2001 <emrecan.senel2001@gmail.com>
2025-03-16 22:43:22 +0300

    Account model for users

---
+++ EmrecNotes/Models/Account.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmrecNotes.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required, StringLength(50)]
+        public string UserName { get; set; }
+
+        [Required]
+        public string PasswordHashed { get; set; }
+    }
+}
