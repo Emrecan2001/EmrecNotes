22e748e1e170565e6c4645949fd83bb8ec88e717
Boethius2001 <emrecan.senel2001@gmail.com>
2025-03-23 18:34:10 +0300

    new migration for role update

---
+++ EmrecNotes/Migrations/20250323150546_rolenotrequired.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmrecNotes.Migrations
+{
+    /// <inheritdoc />
+    public partial class rolenotrequired : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserRole",
+                table: "Account",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserRole",
+                table: "Account",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
