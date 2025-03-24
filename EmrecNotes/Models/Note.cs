82a971227241ab26a8a4c2454cc2f9914a90d2ad
Boethius2001 <emrecan.senel2001@gmail.com>
2025-03-24 23:37:33 +0300

    Note model added

---
+++ EmrecNotes/Models/Note.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmrecNotes.Models
+{
+    public class Note
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public string Title { get; set; }
+
+        [MaxLength(100000)] // contains max 100.000 letters
+        public string? Content { get; set; }
+
+        //foreign key
+        public int UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public Account User { get; set; }
+    }
+}
