e2f021b04eaced6939832df8fa3a473cb38eac61
Boethius2001 <emrecan.senel2001@gmail.com>
2025-03-25 00:50:31 +0300

    filter notes by userid

--- EmrecNotes/Controllers/UsersController.cs
+++ EmrecNotes/Controllers/UsersController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
+using EmrecNotes.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace EmrecNotes.Controllers
 {
@@ -7,13 +10,26 @@ namespace EmrecNotes.Controllers
     [Authorize] //Only Authorizing users can access this controller otherwise they are returning back to LoginPath
     public class UsersController : Controller
     {
-        [Authorize]
-        public IActionResult Index()
-        {
-            // pull cookies (demo)
-            var userName = User.Identity.Name;
 
-            return View("Index", userName);
+        private readonly EmrecNotesContext _context;
+
+        public UsersController(EmrecNotesContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+            // get user id cookie as integer
+            var UserId = Convert.ToInt32(User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+
+            Console.WriteLine(UserId.GetType());
+
+            // list all data from note table with specific user id
+            var Notes = await _context.Note.Where(n => n.UserId == UserId).ToListAsync();
+
+            return View(Notes);
         }
     }
 }
