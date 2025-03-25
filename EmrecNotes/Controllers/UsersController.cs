538887fa6077c3f73982496056274b9d540affd9
Boethius2001 <emrecan.senel2001@gmail.com>
2025-03-26 01:36:27 +0300

    edit controller

--- EmrecNotes/Controllers/UsersController.cs
+++ EmrecNotes/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using EmrecNotes.Data;
+using EmrecNotes.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
 
@@ -18,6 +19,7 @@ namespace EmrecNotes.Controllers
             _context = context;
         }
 
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
 
@@ -30,6 +32,57 @@ namespace EmrecNotes.Controllers
             var Notes = await _context.Note.Where(n => n.UserId == UserId).ToListAsync();
 
             return View(Notes);
+        }
+
+        [Route("create")]
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [Route("create")]
+        [HttpPost]
+        public async Task<IActionResult> Create(Note note)
+        {
+
+            var UserId = Convert.ToInt32(User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+
+            note.UserId = UserId;
+
+            await _context.Note.AddAsync(note);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        [Route("edit/{Id}")]
+        [HttpGet]
+        public async Task<IActionResult> Edit(int Id)
+        {
+            // return note with specific Id
+            return View(await _context.Note.FirstOrDefaultAsync(n => n.Id == Id));
+        }
+
+        // take a break and continue
+        [Route("edit/{Id}")]
+        [HttpPost]
+        public async Task<IActionResult> Edit(int Id, Note UpdatedNote)
+        {
+
+            var Note = await _context.Note.FirstOrDefaultAsync(n => n.Id == Id);
+
+            Console.WriteLine(Id);
+            Console.WriteLine(UpdatedNote.Title);
+            Console.WriteLine(UpdatedNote.Content);
+
+            Note.Title = UpdatedNote.Title;
+            Note.Content = UpdatedNote.Content;
+
+            _context.Update(Note);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
         }
     }
 }
