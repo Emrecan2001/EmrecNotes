b66a2282afdba316e428b1f465585952b81147c9
Boethius2001 <emrecan.senel2001@gmail.com>
2025-03-26 22:42:47 +0300

    nullable title migration

--- EmrecNotes/Migrations/20250326193454_nullabletitle.Designer.cs
+++ EmrecNotes/Migrations/20250326193454_nullabletitle.Designer.cs
@@ -3,6 +3,7 @@ using EmrecNotes.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EmrecNotes.Migrations
 {
     [DbContext(typeof(EmrecNotesContext))]
-    partial class EmrecNotesContextModelSnapshot : ModelSnapshot
+    [Migration("20250326193454_nullabletitle")]
+    partial class nullabletitle
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -63,8 +66,8 @@ namespace EmrecNotes.Migrations
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("Title")
-                        .HasMaxLength(100)
-                        .HasColumnType("nvarchar(100)");
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
 
                     b.Property<int>("UserId")
                         .HasColumnType("int");
--- EmrecNotes/Migrations/20250326193837_nullabletitletwo.Designer.cs
+++ EmrecNotes/Migrations/20250326193837_nullabletitletwo.Designer.cs
@@ -3,6 +3,7 @@ using EmrecNotes.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EmrecNotes.Migrations
 {
     [DbContext(typeof(EmrecNotesContext))]
-    partial class EmrecNotesContextModelSnapshot : ModelSnapshot
+    [Migration("20250326193837_nullabletitletwo")]
+    partial class nullabletitletwo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
